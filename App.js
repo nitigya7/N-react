c60f6e875e155f208e90539eb7417d21fd7c065f
Nitigya Sharma <nitigyasharma728@gmail.com>
2023-11-06 00:52:02 -0600

    update3

--- App.js
+++ App.js
@@ -1,30 +1,24 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
+const heading = React.createElement("h1", {id: "heading", className:"my-head" }, "nevr give up")
 
-const parent = React.createElement("div", {id: "parent"},[
-React.createElement("div",{id: "child"},[
-React.createElement("h1",{class: "heading"}, "h1 tag"),
-React.createElement("h2",{}, "iam h2 tag")
-]),
-React.createElement("div",{id: "child1"},[
-React.createElement("h1",{}, "iam h1 tag"),
-React.createElement("h2",{}, "iam h2 tag")
-]),
-])
-console.log(parent); // object
+console.log(heading)
+const jsxheading = (<h1>this is jsx heading</h1>);
+console.log(jsxheading)
+
+const Comp1 = ()=>{
+    return <h1>this comp1</h1>
+}
+const numb = 1000
+const HeadingComponent = ()=>{
+    return(
+        <>
+      <h2>{numb}</h2>  
+        <Comp1/>
+        <h1 id= "heading"> never give up</h1>
+        </>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
-/*
-<div id = "parent">
-<div id = "child">
-  <h1>iam h1 tag</h1>
-  <h2>iam h2 tag</h2>
-</div>
-<div id = "child2">
-  <h1>iam h1 tag</h1>
-  <h2>iam h2 tag</h2>
-</div>
-</div>
-*/
-
+root.render(<HeadingComponent/>);
